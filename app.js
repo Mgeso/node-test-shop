e59fdd86d68197339d785adfbc8f69a833909b79
Mgeso <106961023+Mgeso@users.noreply.github.com>
2022-08-04 09:00:07 +0100

    setup project env

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-app.use('/products', productRoutes);
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("connected to database"))
+    .catch((error) => console.log(error));
+
+const middleware = (app)=>{
+app.use(morgan('dev'));
+
+
+// Routes which should handle requests
 app.use('/orders', orderRoutes);
 
-module.exports = app;
+app.use('/products', productRoutes);
+}
+
+// app.use((req, res, next) => {
+// const error = new error('Not found');
+// error.status(404);
+// next(error);
+// });
+
+// app.use((error, req, res, next) => {
+// res.status(error.status || 500);
+// res.json({
+//     error: {
+        //   message: error.message
+// }
+// });  
+// });
+
+module.exports= middleware;
